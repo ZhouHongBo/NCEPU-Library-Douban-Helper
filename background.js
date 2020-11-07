17043d4c598fef4ced4520f5bd045302c7d55c43
zhou_hong_bo <15650798020@163.com>
2020-11-07 12:47:07 +0800

    添加样式

--- background.js
+++ background.js
@@ -7,32 +7,34 @@ chrome.runtime.onConnect.addListener((port) => {
             xhr.send();
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
-                    let collectionInfo = parseInfo(xhr.response); // 馆藏信息
+                    let collectionInfo = parseInfo(xhr.response); // 书籍馆藏信息
                     port.postMessage({ message: collectionInfo }); // 发送给content.js
                 }
             }
         });
     }
-
-    function Collection(bookName, authorName, totalNum, validNum, bookUrl) {
-        this.bookName = bookName;
-        this.authorName = authorName;
-        this.totalNum = totalNum;
-        this.validNum = validNum;
-        this.bookUrl = bookUrl;
-    }
-
-    function parseInfo(content){
-        let collections = [];
-        let bookListInfo = $(content).find(".book_list_info"); // 书籍列表
-        let bookUrlInfo = $(bookListInfo).find("p a"); // 书籍链接列表
-        for (let i = 0 ; i < bookListInfo.length; i++){
-            let infoStr = bookListInfo[i].innerText;
-            let bookUrl = "http://opac.ncepu.edu.cn:8080/opac/" + bookUrlInfo[i].attributes.href.nodeValue;
-            let arr = infoStr.trim().split(/\s+/).filter((val)=>val.indexOf("/") === -1);
-            let collection = new Collection(arr[0], arr[3], arr[1], arr[2], bookUrl);
-            collections.push(collection);
-        }
-        return collections;
-    }
 });
+
+// 书籍馆藏信息对象
+function Collection(bookName, authorName, totalNum, validNum, bookUrl) {
+    this.bookName = bookName;
+    this.authorName = authorName;
+    this.totalNum = totalNum; // 馆藏副本
+    this.validNum = validNum; // 可借副本
+    this.bookUrl = bookUrl; // 书籍详情页的链接
+}
+
+// 从查询页面提取需要的书籍馆藏信息
+function parseInfo(content){
+    let collections = [];
+    let bookListInfo = $(content).find(".book_list_info"); // 书籍列表
+    let bookUrlInfo = $(bookListInfo).find("p a"); // 书籍链接列表
+    for (let i = 0 ; i < bookListInfo.length; i++){
+        let infoStr = bookListInfo[i].innerText;
+        let bookUrl = "http://opac.ncepu.edu.cn:8080/opac/" + bookUrlInfo[i].attributes.href.nodeValue;
+        let arr = infoStr.trim().split(/\s+/).filter((val)=>val.indexOf("/") === -1);
+        let collection = new Collection(arr[0], arr[3], arr[1], arr[2], bookUrl);
+        collections.push(collection);
+    }
+    return collections;
+}
--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 ﻿/* 从豆瓣获取标题生成检索url */
 $(document).ready(function () {
-    var bookName = $('h1>span').text(); // 获取书名
+    var bookName = $("h1>span").text(); // 获取书名
     var bookSearchUrl = "http://opac.ncepu.edu.cn:8080/opac/openlink.php?title=" + bookName; // 图书馆检索页网址
     var port = chrome.runtime.connect({ name: "port" }); // 与background.js建立连接
     port.postMessage({ url: bookSearchUrl }); // 发送url到图书馆检索页
@@ -11,20 +11,18 @@ $(document).ready(function () {
     });
 });
 
-/* 向豆瓣添加馆藏信息 */
-function insert(collectionInfo){
-    $('#buyinfo').before('<div class="gray_ad" id="helper"></div>'); // 插入div
-    $('#helper').append('<h2>华电图书馆豆瓣助手</h2>');
-    for(let i = 0; i < collectionInfo.length; i++) {
-        $('#helper').append(collectionInfo[i].bookName);
-        $('#helper').append("<br>")
-        $('#helper').append(collectionInfo[i].authorName);
-        $('#helper').append("<br>")
-        $('#helper').append(collectionInfo[i].totalNum);
-        $('#helper').append("<br>")
-        $('#helper').append(collectionInfo[i].validNum);
-        $('#helper').append("<br>")
-        $('#helper').append(collectionInfo[i].bookUrl);
-        $('#helper').append("<br>")
+// 向豆瓣添加馆藏信息
+function insert(collectionInfo) {
+    $("#buyinfo").before('<div class="gray_ad" id="helper"></div>'); // 插入div
+    $('#helper').append('<h2>华电图书馆豆瓣助手</h2><ul></ul>');
+    for (let i = 0; i < collectionInfo.length; i++) {
+        $("#helper ul").append(`<li>
+        <a target="_blank" href="${collectionInfo[i].bookUrl}">${collectionInfo[i].bookName}</a>
+        <br>
+        <span>${collectionInfo[i].authorName}</span>
+        <br>
+        <span>${collectionInfo[i].totalNum}</span>
+        <span>${collectionInfo[i].validNum}</span>
+    </li>`);
     }
 }
